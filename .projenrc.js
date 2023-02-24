fe6c6d85590de751e3b9b004a66b1c45423c8a95
Galen Dunkleberger <awsgalen@amazon.com>
2023-02-23 21:15:22 -0500

    add projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,21 @@
-const { awscdk } = require('projen');
-const project = new awscdk.AwsCdkConstructLibrary({
+const { execSync } = require('child_process');
+const { typescript } = require('projen');
+const { NodePackageManager } = require('projen/lib/javascript');
+const project = new typescript.TypeScriptProject({
   author: 'Galen Dunkleberger',
   authorAddress: 'awsgalen@amazon.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: `${execSync("npm show 'aws-cdk-lib' version")}`.trim(),
   defaultReleaseBranch: 'main',
   name: 'aws-cdk-state-machine-with-concurrency-controls',
   repositoryUrl: 'git@github.com:aws-samples/aws-cdk-state-machine-with-concurrency-controls.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  packageManager: NodePackageManager.NPM,
+  deps: ['aws-cdk-lib', 'constructs', '@aws-sdk/client-dynamodb', '@aws-sdk/client-sfn', '@aws-sdk/client-sqs', '@types/aws-lambda'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: ['jest'], /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+project.eslint.rules['space-before-blocks'] = 'error'; // require space before blocks
+project.eslint.rules.curly = ['error', 'multi-line', 'consistent']; // require curly for multiline
+project.eslint.rules['max-len'] = ['error', { code: 200, ignoreComments: true }];
+project.eslint.rules['no-var'] = ['error'];
 project.synth();
---
+++ src/runtime/IncrementCount.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
+import { SFNClient, StartExecutionCommand } from '@aws-sdk/client-sfn';
+import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
+import { Context, SQSEvent } from 'aws-lambda';
+
+const ddbClient = new DynamoDBClient({ logger: console });
+const sfnClient = new SFNClient({ logger: console });
+const sqsClient = new SQSClient({ logger: console });
+export const handler = async (event: SQSEvent,
+  // @ts-ignore
+  context: Context): Promise<{ [name: string]: string | number | undefined }> => {
+  console.log(`"Event: ${JSON.stringify(event)}`);
+  for (const record of event.Records) {
+    const body = JSON.parse(record.body);
+    const stepFunctionArn = process.env.STATE_MACHINE_ARN;
+
+    if (stepFunctionArn != null) {
+      try {
+        await ddbClient.send(new UpdateItemCommand({
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            pk: {
+              S: stepFunctionArn,
+            },
+
+          },
+          UpdateExpression: 'ADD #c :inc',
+          ExpressionAttributeNames: {
+            '#c': 'counter',
+          },
+          ExpressionAttributeValues: {
+            ':inc': {
+              N: '1',
+            },
+            ':MAX_CONCURRENCY': {
+              N: process.env.MAX_CONCURRENCY!,
+            },
+          },
+          ConditionExpression: '#c < :MAX_CONCURRENCY',
+
+        }));
+        const sfnResponse = await sfnClient.send(new StartExecutionCommand({
+          stateMachineArn: process.env.STATE_MACHINE_ARN,
+          input: JSON.stringify(body),
+          traceHeader: record.messageId,
+        }));
+        console.log(`Execution ARN: ${sfnResponse.executionArn} started ${sfnResponse.startDate}`);
+      } catch (e) {
+        const error = e as Error;
+        console.error(`Error: ${error}`);
+        if ('ConditionalCheckFailedException' == error.name) {
+
+          try {
+            await sqsClient.send(new SendMessageCommand({
+              DelaySeconds: Math.floor(Math.random() * (120 - 15 + 1) + 15),
+              QueueUrl: process.env.QUEUE_URL,
+              MessageBody: JSON.stringify(body),
+            }));
+          } catch (e1) {
+            const error1 = e1 as Error;
+            console.error(`Error returning message to queue : ${error1}`);
+          }
+        }
+
+      }
+    } else {
+      throw new Error('No step function arn provided');
+    }
+  }
+
+  return {};
+
+};
